60dd68929b6b01b294421b24232dbde2ab762543
hishamRawdata <hisham@rawdatatech.com>
2022-08-19 11:06:04 +0530

    added port env

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((error, req, res, next) => {
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yq0ra.mongodb.net/${process.env.DB_NAME}`)
   .then(() => {
     console.log("Connected");
-    app.listen(5000)
+    app.listen(process.env.PORT || 5000)
   })
   .catch(err => {
     console.log(err);
